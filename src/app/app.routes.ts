bb3e9d7e8d6f64c2be1affb36395190bc77d7c74
bmanriquez <brianmanriquez7@gmail.com>
2024-07-10 13:45:06 -0600

    Se agregan cambios, se muestran componentes en pantalla se empieza a crear navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
-import { Routes } from '@angular/router';
+// src/app/app-routing.module.ts
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { PantallaPrincipalComponent } from './pantalla-principal/pantalla-principal.component';
+import { InventarioComponent } from './inventario/inventario.component';
+import { RegistroComponent } from './registro/registro.component';
 
-export const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: PantallaPrincipalComponent },
+  { path: 'inventory', component: InventarioComponent },
+  { path: 'sales', component: RegistroComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
---
+++ src/app/menu-indice/menu-indice.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-menu-indice',
+  standalone: true,
+  imports: [],
+  templateUrl: './menu-indice.component.html',
+  styleUrl: './menu-indice.component.scss'
+})
+export class MenuIndiceComponent {
+
+}
