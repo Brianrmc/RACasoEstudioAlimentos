1689732cb9e9efb74b3047f0dbb3b8c2dc6d3b31
bmanriquez <brianmanriquez7@gmail.com>
2024-07-10 13:58:02 -0600

    Se agregan estilos globales para acomodar el footer y se adjunta footer a pantalla principal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { MenuIndiceComponent } from './menu-indice/menu-indice.component';
 import { RouterModule } from '@angular/router';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterModule, MenuIndiceComponent],
+  imports: [RouterModule, MenuIndiceComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
